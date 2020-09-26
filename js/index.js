25f10862410960563e3396ddcc4ad22dbf9bd2ed
Alicja Slupska <ala.slupska@gmail.com>
2020-09-26 15:20:17 +0200

    canvas care race in ptogress: iteration 1, 2, 3

--- js/index.js
+++ js/index.js
@@ -5,27 +5,27 @@ window.onload = () => {
 
   function startGame() {
 
-    let context = document.querySelector('canvas').getContext('2d')
+    let context = document.querySelector('canvas').getContext('2d');
 
     let imgRoad = new Image();
     imgRoad.src = 'images/road.png';
 
-    let img = new Image();
+    let imgCar = new Image();
     imgScale = 200/400;
-    img.src = 'https://github.com/ironhack-labs/lab-canvas-race-car/raw/master/images/car.png';
+    imgCar.src = 'images/car.png';
+    
     context.clearRect(0, 0, 1200, 800);
     // context.fillRect(10, 10, 10, 10);
 
     let car = {
       posX: 215,
-      posY: 550
+      posY: 570
     };
 
-    img.onload = () => {
+    imgCar.onload = () => {
       context.drawImage(imgRoad, 0, 0, 500, 700);
-      context.drawImage(img, car.posX,  car.posY, 120*imgScale, 120);
+      context.drawImage(imgCar, car.posX,  car.posY, 120*imgScale, 120);
     };
-
 
 
     document.addEventListener('keydown', event => {
@@ -34,10 +34,10 @@ window.onload = () => {
       } else if (event.keyCode === 39) {
         car.posX += 5;
       }
-      console.log(car.posX)
+      console.log(car.posX);
       context.clearRect(0, 0, 1200, 800);
       context.drawImage(imgRoad, 0, 0, 500, 700);
-      context.drawImage(img, car.posX,  car.posY, 120*imgScale, 120);
+      context.drawImage(imgCar, car.posX,  car.posY, 120*imgScale, 120);
     });
     
  
